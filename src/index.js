4ab24d0a3902165b3e1f963c8ffa7dfc04c4e9e1
HoaiNhu <22521051@gm.uit.edu.vn>
2024-12-19 06:40:40 +0700

    update-user

--- src/index.js
+++ src/index.js
@@ -11,8 +11,15 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000", // URL của frontend
+    credentials: true, // cho phép gửi cookie
+  })
+);
 app.use(bodyParser.json());
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
