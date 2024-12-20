62f07587f666f116d4a0d6ad6eeced68a66f375e
DiemNganNguyenNgoc <22520933@gm.uit.edu.vn>
2024-12-20 11:58:25 +0700

    answer

--- src/models/AnswerModel.js
+++ src/models/AnswerModel.js
@@ -3,22 +3,28 @@ const mongoose = require('mongoose');
 
 const answerSchema = new mongoose.Schema(
     {
-        content: {type: String, required: true},
-        upVoteCount: {type: Number, required: true}, 
-        downVoteCount: {type: Number, required: true},
-        commentCount: {type: Number, required: true},
-        view: {type: Number, required: true}, 
-        reportCount: {type: Number, required: true},
-        active: {type: Boolean, required: true},
+        content: { type: String, required: true },
+        upVoteCount: { type: Number, default: 0 },
+        downVoteCount: { type: Number, default: 0 },
+        answerCount: { type: Number, default: 0 },
+        view: { type: Number, default: 0 },
+        reportCount: { type: Number, default: 0 },
+        active: { type: Boolean, default: true },
+
+        // Trường để lưu nhiều ảnh
+        images: {
+            type: [String], // Mảng các URL ảnh
+            default: []
+        },
 
         //khóa ngoại
-        user: {
-            type: mongoose.Schema.Types.Objectid, 
+        userAns: {
+            type: mongoose.Schema.Types.ObjectId,
             ref: 'User',
             require: true
         },
         question: {
-            type: mongoose.Schema.Types.Objectid, 
+            type: mongoose.Schema.Types.ObjectId,
             ref: 'Question',
             require: true
         },
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,16 @@
 const UserRouter = require("./UserRouter");
 const TagRouter = require("./TagRouter");
 const AdminRouter = require("./AdminRouter");
-const Question = require("./QuestionRouter")
+const Question = require("./QuestionRouter");
+const Answer = require("./AnswerRouter")
+
 
 const routes = (app) => {
   app.use("/api/user", UserRouter);
   app.use("/api/tag", TagRouter);
   app.use("/api/admin", AdminRouter);
   app.use("/api/question", Question);
+ app.use("/api/answer", Answer);
 };
 
 module.exports = routes;
